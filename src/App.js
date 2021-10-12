f335d14205ea275537cf9463e2249674b81b2f02
Dcatnis <khirghizz@gmail.com>
2021-10-12 23:14:50 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react"
+import { BrowserRouter, Route, Link, Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from "./components/Header/Header"
+
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
+        <Header />
 
-    </div>
+
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { Container, Row, Col, Navbar, Nav, } from 'react-bootstrap';
+import { BrowserRouter, Route, Link, Router } from "react-router-dom";
+
+
+import './Header.css';
+
+function Header() {
+
+
+
+    return (
+        <div >
+
+            <header>
+                <Container>
+                    <Row>
+                        <Col sm={2}>
+                            <Navbar.Brand href="#home">
+                                <img
+                                    src="./logo.png"
+                                    width="209"
+                                    height="55"
+                                    className="d-inline-block align-top"
+                                    alt=" logo"
+                                />
+                            </Navbar.Brand>
+                        </Col>
+                        <Col sm={10} >
+                            <Nav className="justify-content-end desktop " activeKey="/home">
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> Principala </Link></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> Portofoliu </Link></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> Despre Noi </Link></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> Servicii </Link></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> Contacte </Link></Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item as="li">
+                                    <Nav.Link> <Link className="nav_links" to="/"> RO/RU </Link></Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+
+                            <div className="mobile">
+                                <div className="mob-lang">
+                                    RU/RO
+
+                                </div>
+                                <div >
+
+                                    <img
+                                        src="../images/burger.png"
+                                        width="30"
+                                        height="30"
+                                        className="d-inline-block align-top"
+                                        alt=" logo"
+                                    />
+                                </div>
+                            </div>
+
+                        </Col>
+
+                    </Row>
+
+                </Container>
+
+
+            </header>
+
+
+
+
+        </div >
+    );
+}
+
+export default Header;
